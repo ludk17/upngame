d62b3b20e811050a9cc4802210252fd58449c3b5
Jhoel Rojas Sánchez <jhoelperucajabamba@gmail.com>
2024-11-01 12:30:09 -0500

    semana12

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,17 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using TMPro;
-
-public class PlayerController : MonoBehaviour
-{
-    public AudioClip jumpSound;
-    private Rigidbody2D rb;
-    private Animator animator;
-    private SpriteRenderer sr;
-    private AudioSource audioSource;
-
-    private const int ANIMATION_IDLE = 0;
+public class PlayerController : MonoBehaviour {
     private const int ANIMATION_RUN = 1;
     private const int ANIMATION_JUMP = 2;
 
@@ -19,26 +6,24 @@ public class PlayerController : MonoBehaviour
 
     private GameObject gameManager;
     private GameObject playerMessage;
+    private Rigidbody2D rb;
+    private SpriteRenderer sr;
+    private Animator animator;
+
+    public float velocityX = 10f;
+    public float velocityY = 10f;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        // acceder a rigidbody
+    void Start() {
         rb = GetComponent<Rigidbody2D>();
         sr = GetComponent<SpriteRenderer>();
         animator = GetComponent<Animator>();
         gameManager = GameObject.Find("GameManager");
         playerMessage = GameObject.Find("PlayerMessage");
-        audioSource = GetComponent<AudioSource>();
-        
     }
 
     // Update is called once per frame
     void Update() {
-        // // modificar el rigidibody
-        
-        // GetKeyUp: se ejecuta cuando se suelta la tecla
-        // GetKeyDown: se ejecuta cuando se presiona la tecla
-        // GetKey: se ejecuta mientras se mantiene presionada la tecla
         if (gravedadEstaActivada) {
             rb.velocity = new Vector2(0, rb.velocity.y);
         } else {
@@ -47,36 +32,45 @@ public class PlayerController : MonoBehaviour
         
         animator.SetInteger("Estado", ANIMATION_IDLE);
 
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            rb.velocity = new Vector2(10, rb.velocity.y);            
-            sr.flipX = false;
+        if (Input.GetKey(KeyCode.RightArrow)) {
+            WalkRight();
         }
 
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            rb.velocity = new Vector2(-10, rb.velocity.y);
-            sr.flipX = true;
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            WalkLeft();
         }
 
         if (Input.GetKey(KeyCode.UpArrow) && !gravedadEstaActivada) {
-                rb.velocity = new Vector2(rb.velocity.x, 10);
+            Jump();
         }
-        
+
         if (Input.GetKey(KeyCode.DownArrow) && !gravedadEstaActivada) {
-            rb.velocity = new Vector2(rb.velocity.x, -10);
+            rb.velocity = new Vector2(rb.velocity.x, -velocityY);
         }
 
         if (rb.velocity.x != 0) {
             animator.SetInteger("Estado", ANIMATION_RUN);
         }
 
-        if (Input.GetKeyUp(KeyCode.Space))
-        {
-            rb.velocity = new Vector2(rb.velocity.x, 10);
-            animator.SetInteger("Estado", ANIMATION_JUMP); // no esta funcionando
+        if (Input.GetKeyUp(KeyCode.Space)) {
+            rb.velocity = new Vector2(rb.velocity.x, velocityY);
         }
-        
+    }
+
+    public void WalkRight() {
+        rb.velocity = new Vector2(velocityX, rb.velocity.y);
+        sr.flipX = false;
+    }
+
+    public void WalkLeft() {
+        rb.velocity = new Vector2(-velocityX, rb.velocity.y);
+        sr.flipX = true;
+    }
+
+    public void Jump() {
+        if (!gravedadEstaActivada) {
+            rb.velocity = new Vector2(rb.velocity.x, velocityY);
+        }
     }
 
     void OnCollisionEnter2D(Collision2D collision) {
@@ -89,12 +83,10 @@ public class PlayerController : MonoBehaviour
         }
 
         if (collision.gameObject.name == "Coin") {
-            // hacer 2
             Debug.Log("Colision con Coin");
         }
 
         if (collision.gameObject.name == "Finish") {
-            // hacer 3
             Debug.Log("Colision con Finish");
         }
 
@@ -103,7 +95,6 @@ public class PlayerController : MonoBehaviour
             gameManagerC.AddKunai(3);
             Destroy(collision.gameObject);
         }
-        
     }
 
     void OnTriggerStay2D(Collider2D collider) {
@@ -121,7 +112,6 @@ public class PlayerController : MonoBehaviour
     }
 
     private void HideMessage() {
-        playerMessage.GetComponent<TextMeshProUGUI>().text = "";
-    }
-
+        playerMessage.GetComponent<TextMeshProUGUI>().text = "";
+    }
 }
